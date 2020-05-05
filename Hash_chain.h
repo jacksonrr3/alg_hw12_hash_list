787fc81070d00c131337059f6448f36b1b6efa46
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2020-05-05 23:03:38 +0300

    Update Hash_chain.h

--- Hash_chain.h
+++ Hash_chain.h
@@ -1,10 +1,10 @@
 #pragma once
 #include "List.h"
 
-template <typename V>
+template <typename K, typename V>
 struct Pair
 {
-	int _key;
+	K _key;
 	V _value;
 
 	Pair() = default;
@@ -16,36 +16,46 @@ struct Pair
 	Pair(int key) :_key(key), _value() {}
 	Pair(int key, V value) :_key(key), _value(value) {}
 
-	friend bool operator==(const Pair<V>& p1, const Pair<V>& p2);
+	friend bool operator==(const Pair& p1, const Pair& p2);
 
 };
 
-template <typename V>
-bool operator==(const Pair<V>& p1, const Pair<V>& p2) {
+template <typename K, typename V>
+bool operator==(const Pair<K, V>& p1, const Pair<K, V>& p2) {
 	return p1._key == p2._key;
 }
 
 
-template <typename T>
+template <typename K, typename V>
 struct Hash_Chain {
 
-	List<Pair<T> >* _hash_table; //äèíàìè÷åñêèé ìàññèâ
+	List<Pair<K, V> >* _hash_table; //äèíàìè÷åñêèé ìàññèâ
 	std::size_t _table_size;
 
+	std::size_t(*_hash)(K key);
+
+	/*
 	std::size_t hash(int i) {
 		return (i % _table_size);
-	}
+	}*/
 
 public:
 	//Hash_Chain():_hash_table() {}
 
-	Hash_Chain(std::size_t size) :_table_size(size), _hash_table(new List<Pair<T> >[size]) {}
+	Hash_Chain(std::size_t size, std::size_t(*hash)(K key)) :
+		_table_size(size),
+		_hash_table(new List<Pair<K, V> >[size]), 
+		_hash(hash) 
+	{}
 
 	~Hash_Chain() = default;
 
-	bool ins(int key, const T& value) {
+	bool ins(K key, const V& value) {
 		bool b = true;
-		for (Node<Pair<T>>* n = _hash_table[hash(key)].getHead(); n; n = n->getNext()) {
+		for (Node<Pair<K, V>>* n = _hash_table[_hash(key) % _table_size].getHead();
+			n; 
+			n = n->getNext()) 
+		{
 			if (n->getItem()._key == key) {
 				n->setItem({key, value});
 				b = false;
@@ -53,35 +63,39 @@ public:
 			}
 		}
 		if (b) {
-			_hash_table[hash(key)].enqueue({ key, value });
+			_hash_table[_hash(key) % _table_size].enqueue({ key, value });
 		}
 		return b;
 	}
 
-	bool del(int key) {
+	bool del(K key) {
 		bool b = false;
 
-		for (Node<Pair<T>>* n = _hash_table[hash(key)].getHead(); n; n = n->getNext()) {
+		for (Node<Pair<K, V>>* n = _hash_table[_hash(key) % _table_size].getHead();
+			n; 
+			n = n->getNext()) 
+		{
 			if (n->getItem()._key == key) {
 				b = true;
-				_hash_table[hash(key)].remove_node(n);
+				_hash_table[_hash(key) % _table_size].remove_node(n);
 				break;
 			}
 		}
 		return b;
 	}
 
-	T search(int key) {
-		Pair<T> temp;
+	V search(K key) {
+		Pair<K, V> temp;
 
-		for (Node<Pair<T>>* n = _hash_table[hash(key)].getHead(); n; n = n->getNext()) {
+		for (Node<Pair<K, V>>* n = _hash_table[_hash(key) % _table_size].getHead();
+			n;
+			n = n->getNext())
+		{
 			if (n->getItem()._key == key) {
 				temp = n->getItem();
 				break;
 			}
 		}
-
 		return temp._value;
 	}
-
 };
